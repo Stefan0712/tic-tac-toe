7b202b55c4468dee83906a77d6aed66295fe6909
Stefan <s.vladulescu@gmail.com>
2022-04-06 20:15:16 +0300

    Created a player factory function, added a new game window and custon names for p1/p2

--- script.js
+++ script.js
@@ -1,12 +1,71 @@
 const startBtn = document.getElementById("startBtn");
 const restartBtn = document.getElementById("restartBtn");
+const saveBtn = document.getElementById("save");
 restartBtn.onclick = function refreshPage(){
     window.location.reload();
 } 
+const Player = (name, option) => {
+    let wins = 0;
+    return {name, option, wins}
+}
+const player1 = Player("PLayer 1", "X");
+const player2 = Player("Player 2", "O");
+
+
+
+
+
+const p1 = document.getElementById("player1");
+const p2 = document.getElementById("player2");
+
+p1.onblur = function(){
+    player1.name = p1.value;
+}
+
+p2.onblur = function(){
+    player2.name = p2.value;
+}
+
+saveBtn.onclick = function(){
+    document.getElementById("player1Name").innerHTML = p1.value;
+    document.getElementById("player2Name").innerHTML = p2.value;
+
+
+    document.querySelector(".startPanel").style.cssText = "display: none";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 var started = false;
-
 startBtn.onclick = function createGameboard(){
     if(started==false){
     started = true;
