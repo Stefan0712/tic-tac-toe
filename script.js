562a7b9f946965eacb5f66fa367e7a3e529061c3
Stefan <s.vladulescu@gmail.com>
2022-04-08 15:10:16 +0300

    Just a test

--- script.js
+++ script.js
@@ -159,6 +159,27 @@ startBtn.onclick = function createGameboard(){
 
 
 };
+/* I' trying a new way to check for winning combination
+let winComb =   [
+                [0,1,2],
+                [3,4,5],
+                [6,7,8],
+                [0,3,6],
+                [1,4,7],
+                [2,5,8],
+                [0,4,8],
+                [4,3,6]
+                ];
+let garr = [
+    [boardArr[0],boardArr[1],boardArr[2]],
+    [boardArr[3],boardArr[4],boardArr[5]],
+    [boardArr[6],boardArr[7],boardArr[8]]
+];
+for(let i=0;i<7;i++){
+    
+}
+*/
+
 let result = 'Restarted';
 function winCheck(boardArr, moveCounter){
     
